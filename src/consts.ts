cd84a948b84b88130f76baa5478d4dca2c413094
Ademola Adigun <ademolaadigun@Ademolas-MacBook-Air.local>
2023-11-25 14:32:17 +0100

    Updated navigation component and added tailwindcss and design library

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Nestedloop';
+export const SITE_DESCRIPTION = 'Nested Loop, an innovative community, serving as a vibrant ecosystem for tech enthusiasts and professionals, all united by the common goal of creating impactful technology solutions for NGOs in Africa. Nested-loop’s unique proposition lies in its ability to coordinate fundraising efforts and secure financial support from multiple sources while Nested Loop handles the technical execution.';
